a690802b8f4cfe7440ab7e5beebaf8a965ed8d9d
whoitandrei <whoitandrei@yandex.ru>
2026-09-04 11:24:39 +0700

    дописал логику в файлах

--- src/wal.cpp
+++ src/wal.cpp
@@ -5,7 +5,9 @@
  
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <fcntl.h>
+#include <mutex>
 #include <sys/socket.h>
 #include <unistd.h>
  
@@ -75,9 +77,57 @@ void Wal::append_record(Op op, const std::string& key, const std::string& value)
     auto value_len = static_cast<uint32_t>(value.size());
     auto record_len = sizeof(uint8_t) + sizeof(key_len) + key_len + sizeof(value_len) + value_len;
 
-    
+    std::string buffer;
+    buffer.reserve(sizeof(record_len) + record_len);
+    append_uint32(buffer, record_len);
+
+    // we can use bitmask here, but left the space for the future updates
+    buffer.push_back(static_cast<char>(op));
+
+    append_uint32(buffer, key_len);
+    buffer.append(key);
+    append_uint32(buffer, value_len);
+    buffer.append(value);
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    write_all(file_.get(), buffer.data(), sizeof(record_len) + record_len);
 }
 
 void Wal::replay(Store& store) const {
+    ::lseek(file_.get(), 0, SEEK_SET);
 
+    while (true) {
+        char len_buf[sizeof(uint32_t)];
+        if (!read_exact(file_.get(), len_buf, sizeof(len_buf))) break;
+
+        uint32_t record_len = read_uint32(len_buf);
+
+        char buffer[record_len];
+        if (!read_exact(file_.get(), buffer, record_len)) break;
+
+        std::string record(buffer);
+        record.resize(record_len);
+
+        size_t pos = 0;
+        auto op = static_cast<Op>(record[pos]);
+        pos += sizeof(uint8_t);
+
+        uint32_t key_len = read_uint32(record.data() + pos);
+        pos += sizeof(key_len);
+        std::string key = record.substr(pos, key_len);
+        pos += key_len;
+
+        uint32_t value_len = read_uint32(record.data() + pos);
+        pos += sizeof(value_len);
+        std::string value = record.substr(pos, value_len);
+
+        switch (op) {
+            case Op::kSet:
+                store.set(key, value);
+                break;
+            case Op::kDel:
+                store.del(key);
+                break;
+        }
+    }
 }
