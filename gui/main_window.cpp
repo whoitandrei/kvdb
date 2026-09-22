c3f6fea43b7c284bbffd6e0677a790bd9972dc80
whoitandrei <whoitandrei@yandex.ru>
2026-09-22 11:27:16 +0700

    update to home screeen

--- gui/main_window.cpp
+++ gui/main_window.cpp
@@ -1,6 +1,5 @@
 #include "main_window.hpp"
 
-#include <QPushButton>
 #include <QStatusBar>
 #include <QTabWidget>
 #include <QVBoxLayout>
@@ -8,18 +7,46 @@
 namespace {
 constexpr int kMainWindowHeight = 400;
 constexpr int kMainWindowWidth = 600;
-}; // namespace
+} // namespace
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     auto* central = new QWidget(this);
-    auto* layout = new QVBoxLayout(central);
+    auto* layoutGeneral = new QVBoxLayout(central);
+
+    auto* toolbarLayout = new QHBoxLayout();
+    host_ = new QLineEdit("localhost");
+    port_ = new QSpinBox();
+    port_->setRange(1, 65535);
+    port_->setValue(8888);
+    connect_button_ = new QPushButton();
+
+    toolbarLayout->addWidget(host_);
+    toolbarLayout->addWidget(port_);
+    toolbarLayout->addWidget(connect_button_);
+
+    layoutGeneral->addLayout(toolbarLayout);
 
     auto* tab = new QTabWidget(central);
     tab->addTab(new QWidget, "console");
-    layout->addWidget(tab);
+    layoutGeneral->addWidget(tab);
 
     setCentralWidget(central);
     setWindowTitle("kvdb gui");
     resize(kMainWindowWidth, kMainWindowHeight);
-    // statusBar()->showMessage("ready");
+
+    applyConnectedState(connected_);
+
+    connect(connect_button_, &QPushButton::clicked, this, &MainWindow::toggleConnection);
+}
+
+void MainWindow::toggleConnection() {
+    connected_ = !connected_;
+    applyConnectedState(connected_);
+}
+
+void MainWindow::applyConnectedState(bool connected) {
+    connect_button_->setText(connected ? "disconnect" : "connect");
+    port_->setEnabled(!connected);
+    host_->setEnabled(!connected);
+    statusBar()->showMessage(connected ? "[INFO] connected" : "[INFO] disconnected");
 }
--- gui/main_window.hpp
+++ gui/main_window.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <QLineEdit>
 #include <QMainWindow>
-
-class QLabel;
+#include <QPushButton>
+#include <QSpinBox>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
   public:
     explicit MainWindow(QWidget* parent = nullptr);
+
+  private slots:
+    void toggleConnection();
+
+  private:
+    void applyConnectedState(bool connected);
+
+  private:
+    QLineEdit* host_ = nullptr;
+    QSpinBox* port_ = nullptr;
+    QPushButton* connect_button_ = nullptr;
+    bool connected_ = false;
 };
